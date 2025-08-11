f3686bd382c251a0d77789aea4a536d54f5751f4
ANNGLORIOUS <mueniannglorious@gmail.com>
2025-08-11 18:31:38 +0300

    updated the styling tailwind

--- chalenges/solution_B/challenge-taskB/src/App.tsx
+++ chalenges/solution_B/challenge-taskB/src/App.tsx
@@ -1,14 +1,12 @@
-
-import './App.css'
+import './index.css';
+import Task from './Task';
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Task />
+    </div>
+  );
 }
 
-export default App
+export default App;
